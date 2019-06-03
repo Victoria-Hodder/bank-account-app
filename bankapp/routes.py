6d528094c0ed57da79569a1c27e1fde86b35de87
Victoria-Hodder <morethangrammar@gmail.com>
2019-06-03 20:34:57 +0200

    created a database.py file and moved data there. Also renamed user_data to users

--- bankapp/routes.py
+++ bankapp/routes.py
@@ -1,16 +1,17 @@
 from flask import request
+
+from bankapp.database import users
 from bankapp import app
 
-user_data = {'user1': ['1234', 2000], 'user2': ['5576', 3000], 'user3': ['2293', 200]}
 
 # http://127.0.0.1:5000/balance?pin=1234&user=user2
 # try to play with the above mentioned url :)
 @app.route('/balance')
 def display_balance():
     pin_number = request.args.get('pin') # instead of writing a pin as argument, we use this line of code. why?
-    user = request.args.get('user') # instead of writing a pin as argument, we use this line of code. why?
-    if pin_number == user_data[user][0]:
-        return 'This is your current balance: {} EUR'.format(user_data[user][1])
+    user_name = request.args.get('user') # instead of writing a pin as argument, we use this line of code. why?
+    if pin_number == users[user_name][0]:
+        return 'This is your current balance: {} EUR'.format(users[user_name][1])
     else:
         return pin_error()
 
@@ -26,8 +27,8 @@ def withdraw_money():
     amount = request.args.get('amount') # instead of writing a pin as argument, we use this line of code. why?
     amount = int(amount) # why does this have to be transformed into integer?
 
-    balance = user_data[user_name][1]
-    if pin_number == user_data[user_name][0]:
+    balance = users[user_name][1]
+    if pin_number == users[user_name][0]:
         if amount <= balance:
             balance = balance - amount
             return 'Withdrew {} EUR. New balance is: {} EUR.'.format(amount, balance)
