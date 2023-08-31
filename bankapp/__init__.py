249cd5b8626ee8c2293aaffd5af30597ef7a627c
victoria-hodder <vrhodder@posteo.net>
2023-08-31 16:25:06 +0200

    update database name

--- bankapp/__init__.py
+++ bankapp/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///side.db' #with pg_admin you can access tables
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' #with pg_admin you can access tables
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
