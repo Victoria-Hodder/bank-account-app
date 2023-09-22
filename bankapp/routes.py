8fd424698271e9ebfaccf69754a43e6a196395be
victoria-hodder <vrhodder@posteo.net>
2023-09-22 09:46:21 +0200

    tidy routes

--- bankapp/routes.py
+++ bankapp/routes.py
@@ -3,99 +3,11 @@ from bankapp import app, db
 from bankapp.models import User
 from bankapp.schemas import UserSchema
 
+
 @app.route('/')
 def home_page():
     return 'This is a bank account app!'
 
-# http://127.0.0.1:5000/balance?pin=1234&name=Victoria
-@app.route('/balance')
-def display_balance():
-    pin_number = request.args.get('pin')
-    name = request.args.get('name')
-    searchedUser = User.query.filter_by(name=name).first()
-
-    if searchedUser: 
-        if pin_number == searchedUser.pin:
-            return 'This is your current balance: {} EUR'.format(searchedUser.balance)
-        else:
-            return pin_error()
-    else:
-        return "User does not exist"
-
-
-def pin_error():
-    return 'Access denied: incorrect PIN.'
-
-
-# http://127.0.0.1:5000/users/1/withdraw
-
-@app.route('/users/<int:user_id>/withdraw', methods=['PATCH'])
-def withdraw(user_id):
-    data = request.get_json(force=True)
-    amount = data['amount']
-    pin = data['pin']
-    if amount > 2000:
-        abort(400, description='You are not allowed to go over 2000 euro daily limit') 
-    else:
-        user = User.query.get_or_404(user_id, "User does not exist")
-        if pin == user.pin: 
-            if amount <= user.balance:
-                user.balance -= amount
-                db.session.commit()
-                user_schema = UserSchema()
-                response = user_schema.dump(user)
-                return jsonify(response)       
-            else:
-                abort(
-                    400,
-                    description="You are not allowed to withdraw more money than you have on your account!",
-                )
-        else:
-            abort(400, description="Pin is not correct")
-
-# http://127.0.0.1:5000/users/1/deposit
-@app.route('/users/<int:user_id>/deposit', methods=['PATCH'])
-def deposit(user_id):
-    data = request.get_json(force=True)
-    amount = data['amount']
-    pin = data['pin']
-    if amount >= 3000:
-        abort(400, description='You are not allowed to go over 3000 euro daily limit') 
-    else:
-        user = User.query.get_or_404(user_id, "User does not exist")
-        if pin == user.pin:
-            user.balance += amount
-            db.session.commit()
-            user_schema = UserSchema()
-            response = user_schema.dump(user)
-            return jsonify(response)   
-        else:
-            abort(400, description="Pin is not correct")
-
-
-@app.route('/users/<int:user_id>/transfer', methods=['PATCH'])
-def transfer(user_id):
-    data = request.get_json(force=True)
-    amount = data["amount"]
-    pin_number = data["pin"]
-    receiver_id = data["receiverId"]
-    if amount > 3000:
-        abort(400, description='You are not allowed to go over 3000 euro daily limit') 
-    else:
-        sender = User.query.get_or_404(user_id, "Sender does not exist")
-        if pin_number == sender.pin:
-            if amount <= sender.balance:
-                receiver = User.query.get_or_404(receiver_id, "Receiver user does not exist")
-                sender.balance -= amount
-                receiver.balance += amount
-                db.session.commit()
-                user_schema = UserSchema()
-                response = user_schema.dump(receiver)
-                return jsonify(response)
-            else:
-                abort(400, description='You dont have enought amount of money in your acount!')
-        else:
-            abort(400, description="Pin is not correct")
 
 @app.route('/users', methods= ['GET'])
 def get_users():
@@ -103,6 +15,15 @@ def get_users():
     users = User.query.all()
     response = user_schema.dump(users)
     return jsonify(response)
+
+
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.get_or_404(user_id, "You do not exist, please try again")
+    user_schema = UserSchema()
+    response = user_schema.dump(user)
+    return jsonify(response)
+
 
 @app.route('/users', methods=['POST'])
 def create_user():
@@ -126,18 +47,10 @@ def create_user():
             response = user_schema.dump(new_user)
             return jsonify(response)
 
-@app.route('/users/<int:user_id>', methods=['GET']) #GET is default
-def get_user(user_id):
-    user = User.query.get_or_404(user_id, "You do not exist, please try again")
-    user_schema = UserSchema()
-    response = user_schema.dump(user)
-    return jsonify(response)
 
-#update resource details
-#PUT /users {'name' = 'James'}
 @app.route('/users/<int:user_id>', methods= ['PUT'])
 def update_user(user_id):
-    data = request.get_json(force=True) #added force=True to resolve NoneType error
+    data = request.get_json(force=True)
     new_name = data['name']
     new_pin = data['pin']
     new_balance = data['balance']
@@ -151,11 +64,81 @@ def update_user(user_id):
     return jsonify(response)
 
 
-#delete resource which I selected
-#DELETE /users/1
+
+# http://127.0.0.1:5000/users/1/withdraw
+@app.route('/users/<int:user_id>/withdraw', methods=['PUT'])
+def withdraw(user_id):
+    data = request.get_json(force=True)
+    amount = data['amount']
+    pin = data['pin']
+    if amount > 2000:
+        abort(400, description='You are not allowed to go over 2000 euro daily limit') 
+    else:
+        user = User.query.get_or_404(user_id, "User does not exist")
+        if pin == user.pin: 
+            if amount <= user.balance:
+                user.balance -= amount
+                db.session.commit()
+                user_schema = UserSchema()
+                response = user_schema.dump(user)
+                return jsonify(response)       
+            else:
+                abort(
+                    400,
+                    description="You are not allowed to withdraw more money than you have on your account!",
+                )
+        else:
+            abort(400, description="Pin is not correct")
+
+
+# http://127.0.0.1:5000/users/1/deposit
+@app.route('/users/<int:user_id>/deposit', methods=['PUT'])
+def deposit(user_id):
+    data = request.get_json(force=True)
+    amount = data['amount']
+    pin = data['pin']
+    if amount >= 3000:
+        abort(400, description='You are not allowed to go over 3000 euro daily limit') 
+    else:
+        user = User.query.get_or_404(user_id, "User does not exist")
+        if pin == user.pin:
+            user.balance += amount
+            db.session.commit()
+            user_schema = UserSchema()
+            response = user_schema.dump(user)
+            return jsonify(response)
+        else:
+            abort(400, description="Pin is not correct")
+
+
+@app.route('/users/<int:user_id>/transfer', methods=['PUT'])
+def transfer(user_id):
+    data = request.get_json(force=True)
+    amount = data["amount"]
+    pin_number = data["pin"]
+    receiver_id = data["receiverId"]
+    if amount > 3000:
+        abort(400, description='You are not allowed to go over 3000 euro daily limit') 
+    else:
+        sender = User.query.get_or_404(user_id, "Sender does not exist")
+        if pin_number == sender.pin:
+            if amount <= sender.balance:
+                receiver = User.query.get_or_404(receiver_id, "Receiver user does not exist")
+                sender.balance -= amount
+                receiver.balance += amount
+                db.session.commit()
+                user_schema = UserSchema()
+                response = user_schema.dump(receiver)
+                return jsonify(response)
+            else:
+                abort(400, description='You dont have enought amount of money in your acount!')
+        else:
+            abort(400, description="Pin is not correct")
+
+
 @app.route('/users/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
     user = User.query.get_or_404(user_id, "You do not exist, please try again")
     db.session.delete(user)
     db.session.commit()
-    return '', 204
+    return f"bye bye {user.name}"
