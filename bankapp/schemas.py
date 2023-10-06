390a0b096a2a7c92726cef7a58c3fecb15ec2641
victoria-hodder <vrhodder@posteo.net>
2023-10-06 16:05:58 +0200

    add simple account model

--- bankapp/schemas.py
+++ bankapp/schemas.py
@@ -1,8 +1,33 @@
 from bankapp import ma
-from bankapp.models import UserModel
-
+from bankapp.models import UserModel, AccountModel
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = UserModel
+ #       include_relationships = True
         load_instance = True
+
+
+class AccountSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = AccountModel
+#        include_fk = True
+        load_instance = True
+
+"""
+# User
+class AuthorSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Author
+
+    id = ma.auto_field()
+    name = ma.auto_field()
+    books = ma.auto_field()
+
+# Accounts
+class BookSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Book
+        include_fk = True
+
+"""
