b6cc074de03a8203c089d65ff4514b072fff92a5
victoria-hodder <vrhodder@posteo.net>
2023-10-20 11:38:07 +0200

    list user accounts and add pin check for opening account

--- bankapp/controllers/accounts.py
+++ bankapp/controllers/accounts.py
@@ -20,6 +20,7 @@ from bankapp import db
 from bankapp.models.account_model import AccountModel
 from flask import jsonify, abort, request
 from bankapp.models.user_model import UserModel
+from bankapp.schemas.user_schema import UserSchema
 
 class AccountService:
 
@@ -28,48 +29,54 @@ class AccountService:
         self.user_id = user_id
 
     def get_accounts(self):
-        users = AccountModel.query.all()
+        accounts = AccountModel.query.all()
         account_schema = AccountSchema(many=True)
-        response = account_schema.dump(users)
+        response = account_schema.dump(accounts)
         return jsonify(response)
     
-    # TODO: only a user can open an account
-    def open_account(self):
+
+    def get_user_accounts(self, user_id):
+        user = UserModel.query.get_or_404(user_id, "You do not exist, please try again")
+        accounts = AccountModel.query.all()
+
+        user_accounts = []
+        for account in accounts:
+            if account.user_id == user.id:
+                user_accounts.append(account)
+
+        account_schema = AccountSchema(many=True)
+        response = account_schema.dump(user_accounts)
+
+        return response
+
+    
+    def open_account(self, user_id):
+        user = UserModel.query.get_or_404(user_id, "You do not exist, please try again")
         data = request.get_json(force=True)
         new_account = data ['account name']
-        user_id = data['user id']
-        # TODO: add pin functionality
+        user_pin = data['pin']
 
-        if not new_account:
-            abort(400, description="To create a new account, you must give it a name.")
-        elif (new_account.lower() != 'current') and (new_account.lower() != 'savings'):
-            abort(400, description="There are only two valid account types: savings or current.")
-        elif not user_id:
-            abort(400, description="Only existing users can create an account")
+        if user_pin == user.pin:
+            if not new_account:
+                abort(400, description="To create a new account, you must give it a name.")
+            elif (new_account.lower() != 'current') and (new_account.lower() != 'savings'):
+                abort(400, description="There are only two valid account types: savings or current.")
+            else:
+                new_account = AccountModel(account_name=new_account, user_id=user_id)
+                db.session.add(new_account)
+                db.session.commit()
+                user_schema = AccountSchema()
+                response = user_schema.dump(new_account)
+                return jsonify(response)
         else:
-            new_account = AccountModel(account_name=new_account, user_id=user_id)
-            db.session.add(new_account)
-            db.session.commit()
-            user_schema = AccountSchema()
-            response = user_schema.dump(new_account)
-            return jsonify(response)
+            abort(400, description="Pin is not correct. Please try again")
     
     def close_account(self):
         account = AccountModel.query.get_or_404(self.account_id, "You cannot delete an account which does not exist")
         user = UserModel.query.get_or_404(self.user_id, "You do not exist, please try again")
         db.session.delete(account)
         db.session.commit()
-        # TODO: add users name to delete message
         return f"You successfully deleted your account. Thanks for using V's banking, {user.name}"
     
 
 from bankapp.schemas.account_schema import AccountSchema
-
-
-"""
-        user = UserModel.query.get_or_404(self.user_id, "You do not exist, please try again")
-        db.session.delete(user)
-        db.session.commit()
-        return f"bye bye {user.name}"
-
-"""
