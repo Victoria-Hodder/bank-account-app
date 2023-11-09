feade68ccad0d696ca0324f77ae3ee1e8dc8ff57
victoria-hodder <vrhodder@posteo.net>
2023-11-09 16:57:00 +0100

    tidying

--- bankapp/controllers/accounts.py
+++ bankapp/controllers/accounts.py
@@ -23,7 +23,7 @@ from bankapp.models.user_model import UserModel
 
 class AccountService:
 
-    # TODO: it is obviously a bad idea to hard code this in real life - put it in an environment variable ;)
+    # Insert your very own admin pin here
     admin_pin = ''
 
     def __init__(self, account_id=None):
