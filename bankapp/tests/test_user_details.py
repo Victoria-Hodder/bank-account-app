303be5e5acec7c019fff1094726224ef1a6f8428
victoria-hodder <vrhodder@posteo.net>
2023-12-07 14:31:54 +0100

    create tests for user details contoller

---
+++ bankapp/tests/test_user_details.py
@@ -0,0 +1,44 @@
+from bankapp import app, db
+import unittest
+import json
+
+from bankapp.models.user_model import UserModel
+from bankapp.tests.test_base import TestBase
+
+
+# Run in this way
+# python3 -m unittest bankapp/tests/*.py
+class TestUser(TestBase):        
+
+    def test_update_user(self):
+        mock_user = self.create_user()
+        mock_update_data = {
+            'name': "Guinea Pig",
+            'address': "34 Old Street",
+            }
+        response = self.client.put(f'/users/{mock_user.id}/update_details', data=json.dumps(mock_update_data))
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(data['name'], 'Guinea Pig')
+        self.assertEqual(data['address'], "34 Old Street")
+
+    def test_update_pin(self):
+        mock_user = self.create_user()
+        mock_update_data = {
+            'pin': '1234',
+            'new pin': '4568'            
+        }
+        response = self.client.put(f'/users/{mock_user.id}/update_pin', data=json.dumps(mock_update_data))
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(data['pin'], '4568')
+
+    def test_update_pin_wrong_pin(self):
+        mock_user = self.create_user()
+        mock_update_data = {
+            'pin': '9999',
+            'new pin': '4568'            
+        }
+        response = self.client.put(f'/users/{mock_user.id}/update_pin', data=json.dumps(mock_update_data))
+        data = response.get_data(as_text=True)
+        self.assertEqual(response.status_code,400)
